5f662949f34d8baf302726d1f4e28b97f5c0be90
BeanDx <balletbrawl@gmail.com>
2025-08-10 02:50:31 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pearde.lol/',   // ровно имя репы, с двумя слэшами по краям
 })
